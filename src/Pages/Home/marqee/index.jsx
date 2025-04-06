324ad20d89d2fa2b413cfc5006f107f1ab8525da
SANKHA SUBHRA <sankhasubhradas1@gmail.com>
2025-04-06 15:31:34 +0530

    changes

--- src/Pages/Home/marqee/index.jsx
+++ src/Pages/Home/marqee/index.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import "./style.css";
 
-// Importing all images from the same relative path
+// Importing all images
 import git from "../../../assets/git.png";
 
-import html from "../../../assets/html.png";
-import css from "../../../assets/css.png";
-import java from "../../../assets/java.png";
-import js from "../../../assets/js.png";
-import linux from "../../../assets/linux.jpg";
-import nodejs from "../../../assets/nodejs.png";
-import reactLogo from "../../../assets/react.png";
-import restapi from "../../../assets/restapi.png";
-import scss from "../../../assets/scss.png";
-import python from "../../../assets/python1.webp";
-import sql from "../../../assets/sql.webp";
-import oraclesql from "../../../assets/oraclesql.png";
-import vitereact from "../../../assets/vitereact.png";
+import html from "/src/assets/html.png";
+import css from "/src/assets/css.png";
+import java from "/src/assets/java.png";
+import js from "/src/assets/js.png";
+import linux from "/src/assets/linux.jpg";
+import nodejs from "/src/assets/nodejs.png";
+import reactLogo from "/src/assets/react.png";
+import restapi from "/src/assets/restapi.png";
+import scss from "/src/assets/scss.png";
+import python from "/src/assets/python1.webp";
+import sql from "/src/assets/sql.webp";
+import oraclesql from "/src/assets/oraclesql.png";
+import vitereact from "/src/assets/vitereact.png";
 
 const techImages = [
   git,
-  
+ 
   html,
   css,
   java,
